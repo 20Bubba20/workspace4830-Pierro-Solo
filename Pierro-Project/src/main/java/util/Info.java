516ef38be24ae5ad53ec7e0b1f75c53db6f8f7a3
Jerron Pierro <jpierro@unomaha.edu>
2024-03-21 19:54:11 -0500

    Fixed Link in most files to redirect to TechExercise instead of LocalHost location

--- Pierro-Project/src/main/java/util/Info.java
+++ Pierro-Project/src/main/java/util/Info.java
@@ -2,7 +2,7 @@ package util;
 
 public interface Info {
 	//public String projName = "TechExercise";
-	public String projName = "Pierro-Project";
+	public String projName = "TechExercise";
 	public String searchWebName = "ServletHibernateDB.html";
 	public String insertWebName = "InsertHibernate.html";
 }
